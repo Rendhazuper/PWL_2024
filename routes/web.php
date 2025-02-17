eb8447ffd8097b70a6bbef643ae4cf8fd5d171ca
Rendha <rendhazuper@gmail.com>
2025-02-17 09:31:29 +0700

    Jobsheet 2 Praktikum Membuat Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'hello World';
+});
+
+Route::get('/world', function (){
+    return 'World';
+});
+
+Route::get ('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get ('/about', function () {
+    return 'NIM : 2341720010 <br> Nama : Rendha Putra Rahmadya';
+});
+
+Route:: get('/user/{name?}',  function ($name='John'){
+    return 'Nama Saya ' .$name;
+});
+
+Route::get ('/posts/{post}/comments/{comment}', function ($postId, $commentId){
+    return 'Pos ke-'.$postId.' Komentar ke-'.$commentId;
+});
+
+Route::get('/articles/{article}', function ($articleId){
+    return 'ini artikel ke-' .$articleId;
+});
+
